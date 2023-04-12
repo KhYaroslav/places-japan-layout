6859a41aabf0159aaea055e5522c142973f467ae
KhYaroslav <icpr.johni@gmail.com>
2023-04-12 18:48:27 +0300

    header mobile css

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom'
 import logo from '../../assets/svg/logo-icon.svg'
 import './Header.css'
 import { useEffect, useState } from 'react'
+import close from '../../assets/svg/close-icon.svg'
 
 export default function Header() {
   const [scrollY, setScrollY] = useState(window.pageYOffset > 0)
+  const [open, setOpen] = useState(false)
 
   const item = [
     { location: '/Articles', title: 'Articles' },
@@ -18,6 +20,8 @@ export default function Header() {
       setScrollY(window.pageYOffset > 0)
     }
   }, [])
+
+  const openNavHandler = () => setOpen((prev) => !prev);
   
   return (
   <div className={`header ${scrollY ? 'header__active' : ''}`}>
@@ -29,7 +33,7 @@ export default function Header() {
           </Link>
         </div>
 
-        <nav className="header__nav">
+        <nav className={`header__nav ${open ? 'header__nav-active' : ''}`}>
           <ul className="header__list">
             {item.map((el) =>
               <li className="header__item">
@@ -37,7 +41,15 @@ export default function Header() {
               </li>
             )}
           </ul>
+          <div className='header__nav-close' onClick={openNavHandler}>
+            <img src={close} alt="close" />
+          </div>
         </nav>
+          <div className='header__burger burger' onClick={openNavHandler}>
+            <span className='burger__line burger__line-first'></span>
+            <span className='burger__line burger__line-second'></span>
+            <span className='burger__line burger__line-third'></span>
+          </div>
       </div>
     </div>
   </div>
